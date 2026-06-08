6e01fe4c2e8aed0679082ca3633a398e95bc7df8
Ivan <262613@edu.p.lodz.pl>
2026-06-08 12:06:41 +0200

    Dodanie całego kodu projektu

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "LogicContainer.h"
+
+// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+int main() {
+   LogicContainer logicContainer;
+   //logicContainer.getVehicleManager().registerCar("12", "volvo", "s40", 4);
+   return 0;
+}
