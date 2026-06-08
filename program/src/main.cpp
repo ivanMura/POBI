2affd52c0e21732717abf1eb5238a7baa3637bf6
Ivan <262613@edu.p.lodz.pl>
2026-06-09 00:07:06 +0200

    Zrobiony main.cpp wszystko chodzi jak maselko

--- program/src/main.cpp
+++ program/src/main.cpp
@@ -1,11 +1,50 @@
 #include <iostream>
+#include <memory>
+#include <boost/smart_ptr/shared_ptr.hpp>
 
-#include "LogicContainer.h"
+#include "managers/RepairOrderManager.h"
+#include "managers/ClientManager.h"
+#include "managers/VehicleManager.h"
+#include "model/Car.h"
+#include "model/Motorcycle.h"
+#include "model/Client.h"
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+using namespace std;
 
 int main() {
-   LogicContainer logicContainer;
-   //logicContainer.getVehicleManager().registerCar("12", "volvo", "s40", 4);
-   return 0;
+    cout<<"Pokaz zarzadzania warsztatem"<<endl;
+
+    ClientManager clientManager;
+    VehicleManager vehicleManager;
+    RepairOrderManager repairOrderManager;
+
+    cout<<"Tworzymy baze danych: "<<endl;
+
+    shared_ptr<Client> client = make_shared<Client>("Jan", "Kowalski", 1);
+    cout<<"Dodajemy klienta: "<<client->getFullName()<<endl;
+
+
+    cout<<"Tworzymy dwa pojazdy: "<<endl;
+
+    shared_ptr<Vehicle> car = vehicleManager.registerCar("123456789", "Koenigsegg", "Gemera",2);
+    shared_ptr<Vehicle> moto = vehicleManager.registerMotorcycle("vinjakis", "Yamaha", "mt-07", "szutrowe");
+
+    client->addVehicle(car);
+    client->addVehicle(moto);
+
+    cout<<"Pojazdy zostaly przypisane do klienta"<<endl;
+
+    shared_ptr<RepairOrder> order = repairOrderManager.createOrder("123456789", "zmianaOpon", car);
+    shared_ptr<RepairOrder> order2 = repairOrderManager.createOrder("vinjakis", "zmianaOleju", moto);
+
+    cout<<"Pojzdy sa w naprawie sprawdzamy czy dziala zabezpieczenie: "<<endl;
+
+    shared_ptr<RepairOrder> order3 = repairOrderManager.createOrder("123456789", "wymianaKlimy", car);
+    if (order3 == nullptr) {
+        cout<<"Nie mozna utworzyc zamowienia, pojazd jest w naprawie"<<endl;
+    }
+    else {
+        cout<<"Mozna utworzyc zamowienie [BLAD W IMPLEMENTACJI!!!!!!]"<<endl;
+    }
+    return 0;
 }
